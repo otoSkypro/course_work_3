f073019d121b45f503c8e783f253be481d312c06
Семья <oc83@mail.ru>
2023-08-14 10:39:11 +0300

    add sorted function

--- utils.py
+++ utils.py
@@ -7,3 +7,12 @@ def load_operations(filename):
 
     return data
 
+
+def sort_operations_by_date(operations):
+    valid_operations = []
+    for operation in operations:
+        if operation.get('date'):
+            valid_operations.append(operation)
+
+    result = sorted(valid_operations, key=lambda operation: operation['date'], reverse=True)
+    return result
